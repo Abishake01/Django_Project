389d0a1c08a6fa3cc7900533c05e6ac71e09e691
Abishake01 <abishake381@gmail.com>
2024-04-11 10:53:46 +0530

    DataList

--- voting/views.py
+++ voting/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 
 # Create your views here.
+arr=['Python','Java','C','C++','Mysql','DataBase','HTML','CSS','JavaScript','TailwindCss','Django','React','Node.JS']
 def vote(request):
-    arr=['Python','Java','C','C++','Mysql','DataBase','HTML','CSS','JavaScript','TailwindCss','Django','React','Node.JS']
     mydic={
         'arr': arr
     }
