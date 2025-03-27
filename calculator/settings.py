208c49d018e81c852e9b010075545eabb7b13868
Abishake01 <abishake381@gmail.com>
2025-03-27 14:00:43 +0530

    final

--- calculator/settings.py
+++ calculator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xm29#$^!0wlg-#d=-lm+botgln%!(7a0(xgq)p(+_ow-fypk!(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh']
+ALLOWED_HOSTS = []
 
 
 # Application definition
